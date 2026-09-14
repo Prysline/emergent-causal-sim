d5f649b987aed90518f8a3236a412aa54096d5af
Boochu <mayhu1993@gmail.com>
2026-09-14 14:12:03 +0800

    v11.12.2: add Social Bid schema version

---
+++ src/social-bid-schema-v1122.js
@@ -0,0 +1,24 @@
+(() => {
+  const W=window.SimWorld;if(!W)return;
+  const VERSION='11.12.2-social-bid-lifecycle';
+  const baseCreateInitialState=W.createInitialState;
+
+  W.VERSION=VERSION;
+  W.createInitialState=(seed)=>{
+    const st=baseCreateInitialState(seed);
+    st.version=VERSION;
+    for(const a of Object.values(st.agents||{})){
+      a.observedSocialBids=[];
+      delete a.pendingInteraction;
+    }
+    return st;
+  };
+  W.SOCIAL_BID_SCHEMA_VERSION=VERSION;
+  if(W.DATA_ZH){
+    W.DATA_ZH.bidId='Social Bid';
+    W.DATA_ZH.bidKind='Bid 類型';
+    W.DATA_ZH.responseToBid='回應 Bid';
+    W.DATA_ZH.visibility='可見性';
+    W.DATA_ZH.owner='Private Owner';
+  }
+})();
