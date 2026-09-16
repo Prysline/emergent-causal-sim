7c1b67ab25d2d158bfdf7e590f289dbac794bb68
Boochu <mayhu1993@gmail.com>
2026-09-17 03:32:44 +0800

    Add relationship foundation schema

---
+++ src/relationship-schema-v1150.js
@@ -0,0 +1,27 @@
+(() => {
+  const W=window.SimWorld;if(!W)return;
+  const VERSION='11.15.0-relationship-foundation';
+  const baseCreateInitialState=W.createInitialState;
+  const ENCOUNTER_WEIGHTS=Object.freeze({
+    acceptTalk:1,
+    talk:1,
+    briefTalkReply:.6,
+    declineTalk:.6,
+    petCat:1,
+    avoidPet:.6,
+    socialNoResponse:.25
+  });
+
+  W.VERSION=VERSION;
+  W.createInitialState=(seed)=>{
+    const st=baseCreateInitialState(seed);
+    st.version=VERSION;
+    for(const a of Object.values(st.agents||{}))a.relationships={};
+    return st;
+  };
+  W.RELATIONSHIP_SCHEMA_VERSION=VERSION;
+  W.RELATIONSHIP_MIN_RELEVANCE=.15;
+  W.RELATIONSHIP_FAMILIARITY_RATE=.08;
+  W.RELATIONSHIP_AFFINITY_RATE=.10;
+  W.RELATIONSHIP_ENCOUNTER_WEIGHTS=ENCOUNTER_WEIGHTS;
+})();
