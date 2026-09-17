72e67149f62e07f567f1fb72a9145a93328646cb
Boochu <mayhu1993@gmail.com>
2026-09-17 14:31:57 +0800

    Define relationship target preference contract

--- src/relationship-schema-v1150.js
+++ src/relationship-schema-v1150.js
@@ -1,13 +1,13 @@
 (() => {
   const W=window.SimWorld;if(!W)return;
-  const VERSION='11.15.0-relationship-foundation';
+  const VERSION='11.15.1-relationship-target-preference';
   const baseCreateInitialState=W.createInitialState;
   const ENCOUNTER_WEIGHTS=Object.freeze({
     acceptTalk:1,
     talk:1,
     briefTalkReply:.6,
     declineTalk:.6,
-    petCat:1,
+    petAnimal:1,
     avoidPet:.6,
     socialNoResponse:.25
   });
@@ -23,5 +23,6 @@
   W.RELATIONSHIP_MIN_RELEVANCE=.15;
   W.RELATIONSHIP_FAMILIARITY_RATE=.08;
   W.RELATIONSHIP_AFFINITY_RATE=.10;
+  W.RELATIONSHIP_TARGET_CAP=8;
   W.RELATIONSHIP_ENCOUNTER_WEIGHTS=ENCOUNTER_WEIGHTS;
 })();
