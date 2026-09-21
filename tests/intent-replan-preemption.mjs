3dffc991bd891c88d3d414623d2c977bca057b71
Boochu <mayhu1993@gmail.com>
2026-09-21 19:30:55 +0800

    Editor-2: advance release expectations (2/4)

--- tests/intent-replan-preemption.mjs
+++ tests/intent-replan-preemption.mjs
@@ -18,7 +18,7 @@ function bind(a,kind,intentKind,extra={}){
 
 E.reset(20260911);
 let st=E.getState();
-assert.equal(st.version,'11.22.1-editor-resident-capabilities');
+assert.equal(st.version,'11.22.2-editor-furniture-definitions');
 assert.equal(E.INTERRUPTION_SCHEMA_VERSION,'11.12.3-replan-preemption');
 noIssues('reset');
 
