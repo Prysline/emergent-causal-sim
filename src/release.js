1c71a2e26c905640d873d2cc653905882ad3416d
Boochu <mayhu1993@gmail.com>
2026-09-22 11:01:15 +0800

    Release: advance furniture traversal geometry marker

--- src/release.js
+++ src/release.js
@@ -1,7 +1,7 @@
 (() => {
   const W=window.SimWorld;
   if(!W?.registerInitialStateInitializer)throw new Error('release.js requires world.js initial-state pipeline.');
-  const VERSION='11.24.0-locomotion-traversal-cost';
+  const VERSION='11.25.0-furniture-traversal-geometry';
 
   W.VERSION=VERSION;
   W.registerInitialStateInitializer('release.version',(st)=>{
