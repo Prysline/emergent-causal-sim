203cf6ad29518cb9c6938fde667ba903a30d1c11
Boochu <mayhu1993@gmail.com>
2026-09-23 19:39:31 +0800

    Release: mark furniture facing semantics correction

--- src/release.js
+++ src/release.js
@@ -1,7 +1,7 @@
 (() => {
   const W=window.SimWorld;
   if(!W?.registerInitialStateInitializer)throw new Error('release.js requires world.js initial-state pipeline.');
-  const VERSION='11.28.0-furniture-local-geometry';
+  const VERSION='11.28.1-furniture-facing-semantics';
 
   W.VERSION=VERSION;
   W.registerInitialStateInitializer('release.version',(st)=>{
